af2bb28096e26eecbe58cbc2a1cf82ef8bca8765
manuel2554 <jmsquien@gmail.com>
2026-03-25 01:33:29 -0400

    Create sw.js
    sgs

---
+++ sw.js
@@ -0,0 +1,11 @@
+self.addEventListener('install', (event) => {
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(clients.claim());
+});
+
+self.addEventListener('fetch', (event) => {
+  // Permite que la app funcione interceptando la red, pero en este caso dejamos que pase directo al servidor
+});
